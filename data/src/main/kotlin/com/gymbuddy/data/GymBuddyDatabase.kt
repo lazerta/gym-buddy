d44d76dbe3fe8c8322b06c5cce2bc1f8bfcb1b45
lazerta <32474132+lazerta@users.noreply.github.com>
2026-09-24 13:43:56 -0400

    Advance Room schema for v3 semantics

--- data/src/main/kotlin/com/gymbuddy/data/GymBuddyDatabase.kt
+++ data/src/main/kotlin/com/gymbuddy/data/GymBuddyDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities=[WorkoutSessionEntity::class,ExerciseExecutionEntity::class,SetEntity::class,AnalysisContextEntity::class,RepEvidenceEntity::class,RepSignalEvidenceEntity::class,RepMetricEvidenceEntity::class,FormObservationEntity::class,CueEventEntity::class,CueResponseEntity::class,CueDeliveryEntity::class,TrackingQualitySummaryEntity::class,SetSummaryEntity::class,WorkoutFlowStateEntity::class,InterruptedSetEntity::class,PersonalCalibrationProfileEntity::class],
-    version=5,
+    version=6,
     exportSchema=true,
 )
 abstract class GymBuddyDatabase:RoomDatabase(){abstract fun evidenceDao():EvidenceDao}
